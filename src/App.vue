8a404517616d7ec16e42f4318e1c7a0c92c6ddd4
Moshi Musukwa <musukwamoshi@gmail.com>
2018-10-27 21:23:58 +0200

    A simple grocery list vue app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Groceries from './components/Groceries.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Groceries
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
 }
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+
 </style>
---
+++ src/components/Groceries.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+       <form @submit.prevent="addSkill">
+
+       <input type="text" placeholder="Add a grocery to the grocery list..." v-model="grocery" v-validate="'min:5'" name="skill" >
+       <transition class="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX" >
+       <p class="alert"  v-if="errors.has('grocery')">{{error.first('grocery')}} </p>
+       </transition>
+
+       </form>
+       
+       <ul>
+       <transition-group enter-active-class="animated bounceInUp" leave-active-class="animated  bounceOutDown">
+       <li v-for="(data, index) in groceries" :key='index'>
+             {{data.grocery}}
+       </li>
+       </transition-group>
+
+       </ul>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Groceries',
+  props: {
+    
+  }
+
+  data(){
+     return {
+      grocery:'';
+      groceries:[
+       {"grocery":"Cooking oil"},
+       {"grocery":"Olive oil"}
+      ]
+     }
+  }
+
+  methods(){
+
+    addGrocery(){
+      
+      this.groceries.push({grocery:this.grocery});
+
+      this.grocery='';
+
+    }
+
+    removeGrocery(){
+
+      this.groceries.splice(id,1);
+
+     
+    }
+
+  }
+}
+</script>
+
+<!-- Adding  "scoped" attribute to limits CSS to this component only -->
+<style scoped>
+
+.alert-in-enter-active {
+  animation: bounce-in .5s;
+}
+.alert-in-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+</style>
